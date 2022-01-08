1a0750574f1848d7ec2cebf9d5b2ee2f473b698e
Juliano Firme <julianofirme23@gmail.com>
2022-01-07 23:37:32 -0300

    Using the context that contains the IP data

--- src/pages/SearchPage/index.tsx
+++ src/pages/SearchPage/index.tsx
@@ -1,18 +1,20 @@
+import IpData from "components/IpData";
 import SearchInput from "components/SearchInput";
 import Title from "components/Title";
+import IpDataProvider from "providers/ipDataProvider";
 import React from "react";
 import { SearchSection } from "./components";
 
 const SearchPage: React.FC = () => {
   return (
-    <>
+    <IpDataProvider>
       <SearchSection>
         <Title />
         <SearchInput />
       </SearchSection>
-      {/* <IpData /> */}
+      <IpData />
       {/* <Map /> */}
-    </>
+    </IpDataProvider>
   );
 };
 
